9cd2f4412319a2e77f3aa3b74c6e4c7d8c9defd4
MariusBratvold <31646703+MariusBratvold@users.noreply.github.com>
2018-03-13 08:14:49 +0100

    hu hb

---
+++ js/kjop.js
@@ -0,0 +1,67 @@
+{
+    "bestilling" : [ null, {
+      "antall" : 1,
+      "kundenr" : 1,
+      "pris" : "13 495",
+      "varenavn" : "Huawei Matebook X"
+    }, {
+      "antall" : 1,
+      "kundenr" : 2,
+      "pris" : "21 990",
+      "varenavn" : "Microsoft Surface Book 2"
+    }, {
+      "antall" : 1,
+      "kundenr" : 3,
+      "pris" : "19 990",
+      "varenavn" : "HP Spectre x360"
+    }, {
+      "antall" : 1,
+      "kundenr" : 4,
+      "pris" : "9 490",
+      "varenavn" : "Microsoft Surface Laptop"
+    }, {
+      "antall" : 1,
+      "kundenr" : 5,
+      "pris" : "15 989",
+      "varenavn" : "Apple Macbook Pro 13"
+    }, {
+      "antall" : 1,
+      "kundenr" : 6,
+      "pris" : "7 490",
+      "varenavn" : "Lenovo Legion Y520T"
+    }, {
+      "antall" : 1,
+      "kundenr" : 7,
+      "pris" : "13 990",
+      "varenavn" : "OMEN av HP 880-121"
+    }, {
+      "antall" : 1,
+      "kundenr" : 8,
+      "pris" : "9 490",
+      "varenavn" : "HP Pavilion Power 580-189"
+    }, {
+      "antall" : 1,
+      "kundenr" : 9,
+      "pris" : "4 990",
+      "varenavn" : "Acer Revo Cube RN-76"
+    }, {
+      "antall" : 1,
+      "kundenr" : 10,
+      "pris" : "20 489",
+      "varenavn" : "Apple iMac 27"
+    } ],
+    "demo" : true
+  }
+  
+
+
+
+
+let btnKjop = document.getElementById("kjop");
+btnKjop.addEventListener("click", kjop);
+function kjop(e) {
+    let varer = Array.from(document.querySelectorAll("input:checked "));
+    let bestilling = varer.map(e => [e.value, e.dataset.name]);
+    console.log(bestilling);
+}
+
